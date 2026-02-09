7a88dca503032f7df3d2899bde0bcdf62df1b2d2
Arpan Pandey <goalat9580@gmail.com>
2026-02-09 20:07:19 +0530

    Update indices_of_two_numbers_so_that_they_add_up_the_target_array.py

--- Python-Lab-experiments-IILM/Experiment-09/indices_of_two_numbers_so_that_they_add_up_the_target_array.py
+++ Python-Lab-experiments-IILM/Experiment-09/indices_of_two_numbers_so_that_they_add_up_the_target_array.py
@@ -1,1 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}   # number -> index
+        
+        for i, num in enumerate(nums):
+            need = target - num
+            
+            if need in mp:
+                return [mp[need], i]
+            
+            mp[num] = i
 
